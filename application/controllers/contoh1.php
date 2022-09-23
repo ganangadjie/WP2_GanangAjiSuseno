48980fac412b2012e38809d3b04b825208c6ffc5
ganangadjie <ganangaji221@gmail.com>
2022-09-23 10:43:03 +0700

    AKU MENCOBA COMMIT KESERATUS KALINYA

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -1,11 +1,15 @@
 <?php
-class Contoh1 extends CI_Controller
-{
-    public function index()
-    {
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contoh1 extends CI_Controller {
+	public function index()
+	{
         echo "<h1>Perkenalkan</h1>";
         echo"Nama saya Ganang Aji Suseno
-            Saya tingga di daerah Gemolong
-            olahraga yang saya sukai adalah Bulutangkis";
-    }
+        Saya tinggal di daerah Gemolong
+        olah raga yang saya sukai adalah
+        Bulutangkis";
+
+		$this->load->view('welcome_message');
+	}
 }
