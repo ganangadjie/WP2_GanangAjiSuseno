b18814cc8aed54fd233ef1ff8ca237ea69c0d668
ganangadjie <ganangaji221@gmail.com>
2022-11-11 16:12:52 +0700

    PERTEMUAN 10

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -92,7 +92,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url');
+$autoload['helper'] = array('url', 'pustaka_helper');
 
 /*
 | -------------------------------------------------------------------
@@ -135,4 +135,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array();
+$autoload['model'] = array('ModelUser', 'ModelBuku');
