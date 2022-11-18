954e504b7e5a57e914115376a00cdf466b2e42a6
ganangadjie <ganangaji221@gmail.com>
2022-11-18 08:41:16 +0700

    KOREKSI

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -36,20 +36,17 @@
         </div>
     </div>
 </div>
-
 <!-- Bootstrap core JavaScript-->
-<script src="<?= base_url('assets/');
-                ?>vendor/jquery/jquery.min.js"></script>
-<script src="<?= base_url('assets/');
-                ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url('assets/vendor/jquery/jquery.min.js'); ?>"></script>
+<script src="<?= base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
 <!-- Core plugin JavaScript-->
-<script src="<?= base_url('assets/'); ?>vendor/jqueryeasing/jquery.easing.min.js"></script>
+<script src="<?= base_url('assets/vendor/jqueryeasing/jquery.easing.min.js'); ?>"></script>
 <!-- Custom scripts for all pages-->
-<script src="<?= base_url('assets/'); ?>js/sb-admin2.min.js"></script>
+<script src="<?= base_url('assets/js/sb-admin2.min.js'); ?>"></script>
 <script>
     $('.custom-file-input').on('change', function() {
         let fileName = $(this).val().split('\\').pop();
-        $(this).next('.custom-filelabel').addClass("selected").html(fileName);
+        $(this).next('.custom-file-label').addClass("selected").html(fileName);
     });
     $(document).ready(function() {
         $("#table-datatable").dataTable();
--- application/views/templates/topbar.php
+++ application/views/templates/topbar.php
@@ -24,9 +24,10 @@
                         <a class="dropdown-item" href="<?= base_url('user'); ?>">
                             <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                             Profile Saya
+                            73
                         </a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="<?= base_url('autentifikasi/logout'); ?>" data-toggle="modal" datatarget="#logoutModal">
+                        <a class="dropdown-item" href="<?= base_url('autentifikasi/logout'); ?>" data-toggle="modal" data-target="#logoutModal">
                             <i class="fas fa-sign-out-alt fa-sm fafw mr-2 text-gray-400"></i>
                             Logout
                         </a>
